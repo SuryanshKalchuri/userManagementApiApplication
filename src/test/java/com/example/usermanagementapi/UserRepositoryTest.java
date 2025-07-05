c983a4a31e3e12d21ff94b7e457356be2541fb7b
Suryansh Dev Kalchuri <suryanshdev999@gmail.com>
2025-07-05 10:29:44 +0530

    ✅ Add tests for UserRepository

--- src/test/java/com/example/usermanagementapi/UserRepositoryTest.java
+++ src/test/java/com/example/usermanagementapi/UserRepositoryTest.java
@@ -15,4 +15,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void setUp(){
+        userRepository.deleteAllInBatch();
+        user1 = new User("Alice Wonderland", "alice@example.com");
+        user2 = new User("Bob The Builder", "bob@example.com");
+        entityManager.persist(user1);
+        entityManager.persist(user2);
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @Test
+    void testFindByEmailFound(){
+        Optional<User> foundUser = userRepository.findByEmail(user1.getEmail());
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getEmail()).isEqualTo(user1.getEmail());
+        assertThat(foundUser.get().getId()).isEqualTo(user1.getId());
+    }
 }
