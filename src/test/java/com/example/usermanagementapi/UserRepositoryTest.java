4d2fa21a6b48c937f13886cf973c2f8507b2ae4f
Suryansh Dev Kalchuri <suryanshdev999@gmail.com>
2025-07-05 10:29:26 +0530

    ✅ Add tests for UserRepository

--- src/test/java/com/example/usermanagementapi/UserRepositoryTest.java
+++ src/test/java/com/example/usermanagementapi/UserRepositoryTest.java
@@ -1,4 +1,18 @@
 package com.example.usermanagementapi;
 
+import org.junit.jupiter.api.Test;
+import com.example.usermanagementapi.model.User;
+import com.example.usermanagementapi.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
 }
